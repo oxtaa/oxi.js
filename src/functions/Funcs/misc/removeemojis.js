c742353d420ad3494dc1ac793841276b560b8599
oxta <75595163+oxtaa@users.noreply.github.com>
2023-07-12 17:29:12 -0300

    Add files via upload

---
+++ src/functions/Funcs/misc/removeemojis.js
@@ -0,0 +1,20 @@
+//added
+module.exports = async d => {
+    const data = d.util.aoiFunc(d);
+    const [message] = data.inside.splits;
+  
+    const emojiRegex = /(\s+)?([\u2700-\u27BF]|[\uD83C]|[\uDC00-\uDFFF]|\uD83D|[\uDC00-\uDE4F]|<a?:[\w]+:[\d]+>)/g;
+    const modifiedText = message.replace(emojiRegex, function(match, space, emoji) {
+      if (space && !/\S/.test(message.slice(message.indexOf(match) + match.length))) {
+        return space.trim();
+      } else {
+        return "";
+      }
+    });
+  
+    data.result = modifiedText
+  
+    return {
+      code: d.util.setCode(data),
+    };
+    }
