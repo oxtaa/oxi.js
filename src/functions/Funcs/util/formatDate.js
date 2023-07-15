7a91aaa2fbe5303c7dcbfb0663423646c2d81c1b
oxta <75595163+oxtaa@users.noreply.github.com>
2023-07-15 12:34:40 -0300

    Update formatDate.js

--- src/functions/Funcs/util/formatDate.js
+++ src/functions/Funcs/util/formatDate.js
@@ -1,34 +1,16 @@
-const formatDate = require("../../../handler/FormatDate");
+//modified
+module.exports = async d => {
+    data = d.util.aoiFunc(d);
+    const axios = require('axios');
+    const [timestamp, format = "MM/DD/YY"] = data.inside.splits
+    try {
+    const response = await axios.get(`https://oxiapi.0xtag4.repl.co/json/formatdate?timestamp=${timestamp}&format=${encodeURIComponent(format)}`);
 
-module.exports = (d) => {
-    const code = d.command.code,
-        r = code.split("$formatDate").length - 1,
-        inside = code.split("$formatDate")[r].after();
-
-    const err = d.inside(inside);
-
-    if (err) return d.error(err);
-
-    let [
-        date = Date.now().toLocaleString("en-us", {timeZone: d.timezone}),
-        format = "dddd, DD MMMM YYYY",
-    ] = inside.splits;
-    const checkIsValid = new Date(
-        isNaN(Number(date)) ? date : Number(date)
-    );
-
-    if (isNaN(checkIsValid.getTime())) {
-        return d.error(
-            `\`${d.func}: Invalid date in ${inside}\``
-        );
-    }
-
+    data.result = response.data.result;
+  } catch (error) {
+    return d.aoiError.fnError(d, 'custom', {}, `Invalid timestamp at ${timestamp}`);
+  }
     return {
-        code: code.replaceLast(
-            `$formatDate${inside}`,
-            format.replace(/\w+/g, (value) =>
-                formatDate(value, checkIsValid, d.timezone)
-            )
-        ),
-    };
-};
+      code: d.util.setCode(data)
+    }
+  }
