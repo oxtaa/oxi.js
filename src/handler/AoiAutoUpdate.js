7594151cd71beb51db838d8aea9952234a55afba
Cyberghxst <alphadroidl20@gmail.com>
2023-07-17 11:16:42 -0600

    Updated AutoUpdate - Now AutoUpdate looks for oxi.js latest version instead of aoi.js latest version.

--- src/handler/AoiAutoUpdate.js
+++ src/handler/AoiAutoUpdate.js
@@ -3,36 +3,36 @@ const axios = require("axios").default;
 const json = require("../../package.json");
 module.exports = async () => {
     console.log(
-        "aoi.js AutoUpdate: \u001b[33mExecuting a contact with API...\u001b[0m",
+        "oxi.js AutoUpdate: \u001b[33mExecuting a contact with API...\u001b[0m",
     );
 
     try {
-        const res = await axios.get("https://api.leref.repl.co/package/version"); //This will change in v6 to be native.
-        if (json.version !== res.data.version) {
+        const res = await axios.get("https://registry.npmjs.com/oxi.js");
+        if (json.version !== res.data["dist-tags"].latest) {
             console.log(
-                "aoi.js AutoUpdate: \u001b[33mAvailable version v" +
+                "oxi.js AutoUpdate: \u001b[33mAvailable version v" +
                 res.data.version +
                 " ready to install.\u001b[0m",
             );
 
             // Install initiate
             console.log(
-                "aoi.js AutoUpdate: \u001b[33m Installing version...\u001b[0m",
+                "oxi.js AutoUpdate: \u001b[33m Installing version...\u001b[0m",
             );
-            const Process = exec("npm i aoi.js@latest", (error) => {
+            const Process = exec("npm i oxi.js@latest", (error) => {
                 if (error)
                     return console.error(
-                        "aoi.js AutoUpdate: \u001b[31mERR!\u001b[0m " +
+                        "oxi.js AutoUpdate: \u001b[31mERR!\u001b[0m " +
                         error.message,
                     );
 
                 console.log(
-                    "aoi.js AutoUpdate: \u001b[32mSuccessfully Installed aoi.js v" +
+                    "oxi.js AutoUpdate: \u001b[32mSuccessfully Installed oxi.js v" +
                     res.data.version +
                     ".\u001b[0m",
                 );
                 console.log(
-                    "aoi.js AutoUpdate: Commencing 'RESTART' in 3 Seconds...",
+                    "oxi.js AutoUpdate: Commencing 'RESTART' in 3 Seconds...",
                 );
 
                 setTimeout(Reboot, 3000);
@@ -48,12 +48,12 @@ module.exports = async () => {
             });
         } else {
             console.log(
-                "aoi.js AutoUpdate: \u001b[32mVersion is up-to-date.\u001b[0m",
+                "oxi.js AutoUpdate: \u001b[32mVersion is up-to-date.\u001b[0m",
             );
         }
     } catch (error) {
         console.warn(
-            "aoi.js AutoUpdate: \u001b[31mUnexpected error when trying to reach API.\u001b[0m",
+            "oxi.js AutoUpdate: \u001b[31mUnexpected error when trying to reach API.\u001b[0m",
         );
     }
 };
@@ -70,7 +70,7 @@ function Reboot() {
         process.exit();
     } catch (e) {
         console.error(
-            `aoi.js AutoUpdate: \u001b[31mERR!\u001b[0m Failed to commence 'RESTART', ${e.message}`,
+            `oxi.js AutoUpdate: \u001b[31mERR!\u001b[0m Failed to commence 'RESTART', ${e.message}`,
         );
     }
 }
