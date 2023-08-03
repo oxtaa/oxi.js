97b867702aa8eb96729ce2ce705f1825bec1a2cf
oxta <75595163+oxtaa@users.noreply.github.com>
2023-08-03 15:26:00 -0300

    Add files via upload

---
+++ src/functions/Funcs/util/formatDate.js
@@ -0,0 +1,16 @@
+//modified
+module.exports = async d => {
+    data = d.util.aoiFunc(d);
+    const axios = require('axios');
+    const [timestamp, format = "MM/DD/YY", lowercase = "false"] = data.inside.splits
+    try {
+    const response = await axios.get(`https://oxiapi.0xtag4.repl.co/json/formatdate?timestamp=${timestamp}&format=${encodeURIComponent(format)}&uselowcase=${lowercase}`);
+
+    data.result = response.data.result;
+  } catch (error) {
+    return d.aoiError.fnError(d, 'custom', {}, `Invalid timestamp at ${timestamp}`);
+  }
+    return {
+      code: d.util.setCode(data)
+    }
+  }
